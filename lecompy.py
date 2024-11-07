8b0f155624b90a8cf2392d60f8a77a27bdb3cc69
bsohara <brunosoharadev@gmail.com>
2024-11-07 17:13:34 -0300

    LCPY-007(development): putting the registration form window

--- lecompy.py
+++ lecompy.py
@@ -2,7 +2,7 @@ import PySimpleGUI as psg
 
 headings = ['Código LECOM', 
             'OLT',
-            'ONT_ONU',
+            'ONT ou ONU',
             'Roteador',
             'FSAN/Nº série',
             'Início',
@@ -21,26 +21,57 @@ data = [
      '31/10/2024',
      'Rhyan',
      'Montado',
-     'Testes LPNet - Operações']
+     'Testes LPNet - Operações'],
+     ['123.456',
+     'ZTE 650',
+     'Zyxel PMG2005',
+     'ZTE ZXHN H199A',
+     'ZTEGDFFE453C1',
+     '30/04/2024',
+     '01/05/2024',
+     'Bruno',
+     'Solicitado',
+     '-']
 ]
 
 layout = [
     [psg.Table(values=data, headings=headings, key='table_alias', justification='right')],
-    [psg.Button('Visualizar'), psg.Button('Sair'), psg.Button('Registrar monitoramento')]
+    [psg.Button('Visualizar'), psg.Button('Registrar monitoramento')]
 ]
+
+"""
+form_layout = [
+
+]
+"""
+
+
 
 window = psg.Window('LECOMPY', layout)
 
 while True:
     event, values = window.read()
-    
-    selected_row_idx = values['table_alias']
+
+    if event == psg.WINDOW_CLOSED or event == 'Sair':
+        break
+
+    if event == 'Visualizar':
+        # Check if a row is selected
+        selected_row_idx = values.get('table_alias', None)
         
-    if selected_row_idx:
-        # Get the selected row index and retrieve corresponding data from the data list
-        selected_row_data = data[selected_row_idx[0]]  # Since `selected_row_idx` is a list of indices
-        psg.popup('Linha selecionada:', selected_row_data)
-    else:
-        psg.popup('Nenhuma linha selecionada')
+        if selected_row_idx and len(selected_row_idx) > 0:
+            # Get the selected row index and retrieve corresponding data from the data list
+            selected_row_data = data[selected_row_idx[0]]  # Since `selected_row_idx` is a list of indices
+            
+            # Prepare header and data pairs
+            header_data = "\n".join([f"{head}: {selected_row_data[i]}" for i, head in enumerate(headings)])
+            
+            # Show the data in the popup
+            psg.popup('Linha selecionada:', header_data)
+        else:
+            psg.popup('Nenhuma linha selecionada')
+
+    if event == 'Registrar monitoramento':
+        registration_form_window = psg.Window('LECOMPY - Registro de monitoramento')
 
 window.close()
